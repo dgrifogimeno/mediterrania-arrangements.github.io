2be7ec6e56e04c423dc2ec6fa80d22f915d9d4ef
dgrifogimeno <david.grifo.gimeno@gmail.com>
2024-07-15 20:24:19 +0200

    actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,16 @@ import react from "@astrojs/react";
 import CompressionPlugin from "vite-plugin-compression";
 import sitemap from "@astrojs/sitemap";
 
-//export const siteUrl = "https://cybernetic-free-version.netlify.app";
 const LOCALHOST_URL = `http://localhost:4321`
 
-const LIVE_URL = 'https://dgrifogimeno/mediterrania-arrangements.github.io';
+export const siteUrl = 'https://dgrifogimeno/mediterrania-arrangements.github.io';
 
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
 
 if (isBuild) {
-    BASE_URL = LIVE_URL;
+    BASE_URL = siteUrl;
 }
 
 const date = new Date().toISOString();
