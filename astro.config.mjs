54ec24dbaca5f9bc23ed801bc9cb4589835320b1
dgrifogimeno <david.grifo.gimeno@gmail.com>
2024-07-15 20:07:30 +0200

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,27 @@ import react from "@astrojs/react";
 import CompressionPlugin from "vite-plugin-compression";
 import sitemap from "@astrojs/sitemap";
 
-export const siteUrl = "https://cybernetic-free-version.netlify.app";
+//export const siteUrl = "https://cybernetic-free-version.netlify.app";
+const LOCALHOST_URL = `http://localhost:4321`
+
+const LIVE_URL = 'https://dgrifogimeno/mediterrania-arrangements.github.io';
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+    BASE_URL = LIVE_URL;
+}
 
 const date = new Date().toISOString();
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dgrifogimeno.github.io',
-    base: '/mediterrania-arrangements',
-
+    server: { port: 4321 },
+    site: BASE_URL,
     integrations: [
         react(),
-        sitemap({
+        sitemap(/*{
             serialize(item) {
                 // Default values for pages
                 item.priority = siteUrl + "/" === item.url ? 1.0 : 0.9;
@@ -38,7 +48,7 @@ export default defineConfig({
                 // }
                 return item;
             },
-        }),
+        }*/),
     ],
     renderers: ["@astrojs/renderer-react"],
     prerender: true,
